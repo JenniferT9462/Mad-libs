6ecf5a35424f93f29e7d3ab3bda99eea7cd8951c
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-08-26 18:51:35 -0500

    Add comments

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
-// function clickNext() {
-//   console.log("Next button clicked!");
-//   var pNoun = document.getElementById("pNounInput").value;
-//   var noun1 = document.getElementById("nounInput1").value;
-//   var noun2 = document.getElementById("nounInput2").value;
-//   var song = document.getElementById("songInput").value;
-//   var verb = document.getElementById("verbInput").value;
-
-//   console.log(pNoun);
-
-//   var madLibDisplay = document.getElementById("displayOutput");
-//   var madLib = "Learning to drive is a tricky process. There " +
-//                "are a few rules you must follow.\n\n" +
-//                "1. Keep two " + pNoun +
-//                " on the steering wheel at all times.\n\n" +
-//                "2. Step on the " + noun1 +
-//                " to speed up and the " + noun2  +
-//                " to slow down.\n\n3. Your parents will just " +
-//                "LOVE it if you play " + song  +
-//                " on the radio.\n\n4. Make sure to honk " +
-//                "your horn when you see " + verb  +
-//                " on a street sign.";
-
-//    // ✅ Save the mad lib into localStorage
-//   localStorage.setItem("madLibStory", madLib);
-
-// }
-
-// Code.org code....
+//=====Code.org code======
 
 onEvent("nextBtn", "click", function () {
   var pluralNoun = getValue("pNounInput");
@@ -58,5 +30,34 @@ onEvent("nextBtn", "click", function () {
   setText("displayOutput", madLib);
   document.getElementById("madLibInputs").classList.add("d-none");
   document.getElementById("madLibOutput").classList.remove("d-none");
-  
 });
+
+//======JavaScript======
+
+// function clickNext() {
+//   console.log("Next button clicked!");
+//   var pNoun = document.getElementById("pNounInput").value;
+//   var noun1 = document.getElementById("nounInput1").value;
+//   var noun2 = document.getElementById("nounInput2").value;
+//   var song = document.getElementById("songInput").value;
+//   var verb = document.getElementById("verbInput").value;
+
+//   console.log(pNoun);
+
+//   var madLibDisplay = document.getElementById("displayOutput");
+//   var madLib = "Learning to drive is a tricky process. There " +
+//                "are a few rules you must follow.\n\n" +
+//                "1. Keep two " + pNoun +
+//                " on the steering wheel at all times.\n\n" +
+//                "2. Step on the " + noun1 +
+//                " to speed up and the " + noun2  +
+//                " to slow down.\n\n3. Your parents will just " +
+//                "LOVE it if you play " + song  +
+//                " on the radio.\n\n4. Make sure to honk " +
+//                "your horn when you see " + verb  +
+//                " on a street sign.";
+
+//    // ✅ Save the mad lib into localStorage
+//   localStorage.setItem("madLibStory", madLib);
+
+// }
--- screen2.js
+++ screen2.js
@@ -1,3 +1,19 @@
+//=====Code.org code=====
+
+onEvent("playBtn", "click", function () {
+  // setScreen("screen1");
+  setText("pNounInput", "");
+  setText("nounInput1", "");
+  setText("nounInput2", "");
+  setText("songInput", "");
+  setText("verbInput", "");
+  document.getElementById("madLibInputs").classList.remove("d-none");
+  document.getElementById("madLibOutput").classList.add("d-none");
+  
+});
+
+// ======JavaScript=====
+
 // window.onload = function() {
 //   // Get Madlib from localstorage
 //   var madLib = localStorage.getItem("madLibStory");
@@ -20,16 +36,3 @@
 
 
 
-// Code.org code...
-
-onEvent("playBtn", "click", function () {
-  // setScreen("screen1");
-  setText("pNounInput", "");
-  setText("nounInput1", "");
-  setText("nounInput2", "");
-  setText("songInput", "");
-  setText("verbInput", "");
-   document.getElementById("madLibInputs").classList.remove("d-none");
-  document.getElementById("madLibOutput").classList.add("d-none");
-  
-});
