80525ecbd9362977e69b3fa15a3589c19ba12c09
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-08-26 16:20:29 -0500

    Got it working with the code.org code.

--- index.js
+++ index.js
@@ -1,53 +1,61 @@
-function clickNext() {
-  console.log("Next button clicked!");
-  var pNoun = document.getElementById("pNounInput").value;
-  var noun1 = document.getElementById("nounInput1").value;
-  var noun2 = document.getElementById("nounInput2").value;
-  var song = document.getElementById("songInput").value;
-  var verb = document.getElementById("verbInput").value;
-  
-  console.log(pNoun);
+// function clickNext() {
+//   console.log("Next button clicked!");
+//   var pNoun = document.getElementById("pNounInput").value;
+//   var noun1 = document.getElementById("nounInput1").value;
+//   var noun2 = document.getElementById("nounInput2").value;
+//   var song = document.getElementById("songInput").value;
+//   var verb = document.getElementById("verbInput").value;
 
-  var madLibDisplay = document.getElementById("displayOutput");
-  var madLib = "Learning to drive is a tricky process. There " +
-               "are a few rules you must follow.\n\n" +
-               "1. Keep two " + pNoun +
-               " on the steering wheel at all times.\n\n" +
-               "2. Step on the " + noun1 +
-               " to speed up and the " + noun2  +
-               " to slow down.\n\n3. Your parents will just " + 
-               "LOVE it if you play " + song  +
-               " on the radio.\n\n4. Make sure to honk " +
-               "your horn when you see " + verb  +
-               " on a street sign.";
+//   console.log(pNoun);
 
+//   var madLibDisplay = document.getElementById("displayOutput");
+//   var madLib = "Learning to drive is a tricky process. There " +
+//                "are a few rules you must follow.\n\n" +
+//                "1. Keep two " + pNoun +
+//                " on the steering wheel at all times.\n\n" +
+//                "2. Step on the " + noun1 +
+//                " to speed up and the " + noun2  +
+//                " to slow down.\n\n3. Your parents will just " +
+//                "LOVE it if you play " + song  +
+//                " on the radio.\n\n4. Make sure to honk " +
+//                "your horn when you see " + verb  +
+//                " on a street sign.";
 
-    
-   // ✅ Save the mad lib into localStorage
-  localStorage.setItem("madLibStory", madLib);
-   
-}
+//    // ✅ Save the mad lib into localStorage
+//   localStorage.setItem("madLibStory", madLib);
 
+// }
 
 // Code.org code....
 
-// onEvent("nextBtn", "click", function(){
-//   // setScreen("screen2");
-//   var pluralNoun = getValue("pNounInput");
-//   var noun1 = getValue("nounInput1");
-//   var noun2 = getValue("nounInput2");
-//   var song = getValue("songInput");
-//   var verb = getValue("verbInput");
+onEvent("nextBtn", "click", function () {
+  var pluralNoun = getValue("pNounInput");
+  var noun1 = getValue("nounInput1");
+  var noun2 = getValue("nounInput2");
+  var song = getValue("songInput");
+  var verb = getValue("verbInput");
 
-//   var madLib = "Learning to drive is a tricky process. There " +
-//              "are a few rules you must follow.\n\n" +
-//              "1. Keep two " + pluralNoun + " on the steering " +
-//              "wheel at all times.\n\n2. Step on the " + noun1  +
-//              " to speed up and the " + noun2 +  " to slow down.\n\n" +
-//              "3. Your parents will just LOVE it if you play " +
-//              song + " on the radio.\n\n4. Make sure to honk " +
-//              "your horn when you see " +  verb +  " on a street sign.";
+  console.log(pluralNoun);
+  var madLib =
+    "Learning to drive is a tricky process. There " +
+    "are a few rules you must follow.\n\n" +
+    "1. Keep two " +
+    pluralNoun +
+    " on the steering " +
+    "wheel at all times.\n\n2. Step on the " +
+    noun1 +
+    " to speed up and the " +
+    noun2 +
+    " to slow down.\n\n" +
+    "3. Your parents will just LOVE it if you play " +
+    song +
+    " on the radio.\n\n4. Make sure to honk " +
+    "your horn when you see " +
+    verb +
+    " on a street sign.";
 
-  
-//   setText("messageArea", madLib);
-// });
+  console.log(madLib);
+  setText("displayOutput", madLib);
+  document.getElementById("madLibInputs").classList.add("d-none");
+  document.getElementById("madLibOutput").classList.remove("d-none");
+});
--- screen2.js
+++ screen2.js
@@ -1,32 +1,35 @@
-window.onload = function() {
-  // Get Madlib from localstorage
-  var madLib = localStorage.getItem("madLibStory");
-  console.log(madLib);
+// window.onload = function() {
+//   // Get Madlib from localstorage
+//   var madLib = localStorage.getItem("madLibStory");
+//   console.log(madLib);
 
-  if (madLib) {
-    document.getElementById("displayOutput").textContent = madLib;
-  }
-}
+//   if (madLib) {
+//     document.getElementById("displayOutput").textContent = madLib;
+//   }
+// }
 
-function clickPlay() {
-  console.log("Play Button Clicked!");
+// function clickPlay() {
+//   console.log("Play Button Clicked!");
 
-  // Clear from local storage
-  localStorage.removeItem("madLibStory");
+//   // Clear from local storage
+//   localStorage.removeItem("madLibStory");
 
-  //Go back to index.html
-  window.location.href = "./index.html";
-}
+//   //Go back to index.html
+//   window.location.href = "./index.html";
+// }
 
 
 
 // Code.org code...
 
-// onEvent("playBtn", "click", function () {
-//   // setScreen("screen1");
-//   setText("pNounInput", "");
-//   setText("nounInput1", "");
-//   setText("nounInput2", "");
-//   setText("songInput", "");
-//   setText("verbInput", "");
-// });
+onEvent("playBtn", "click", function () {
+  // setScreen("screen1");
+  setText("pNounInput", "");
+  setText("nounInput1", "");
+  setText("nounInput2", "");
+  setText("songInput", "");
+  setText("verbInput", "");
+   document.getElementById("madLibInputs").classList.remove("d-none");
+  document.getElementById("madLibOutput").classList.add("d-none");
+  
+});
